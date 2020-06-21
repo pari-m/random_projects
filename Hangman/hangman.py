1bd3e851c2e376457f642b2014efc4d3a074f580
Parikshith <pari@133t.net>
2020-06-21 23:01:18 +0530

    added error handling

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,4 +1,4 @@
-# Write your code here
+
 import random
 
 print('H A N G M A N\n')
@@ -16,6 +16,7 @@ computed_word_with_blanks = list(temp_word)
 correctely_guessed_words = list()
 word_aldready_guessed = 0
 first_word_guess = 0
+all_valid_guessed_words = list()
 
 no_of_attempts = 0
 
@@ -24,19 +25,40 @@ no_of_attempts = 0
 while no_of_attempts < 8:
     letter_present = 1
     no_of_attempts += 1
-    if first_word_guess == 1:
+    if first_word_guess == 1 :
         no_of_attempts += -1
     first_word_guess = 0
     word_aldready_guessed = 0
     print(*computed_word_with_blanks,sep = "")
     letter = input('Input a letter:')
+    if len(letter) != 1:  # Input should have length of 1
+            print("You should input a single letter")
+            no_of_attempts += -1
+            print()
+            continue
+    elif letter.islower() != True:  # Input should be in lowercase
+            print("It is not an ASCII lowercase letter")
+            no_of_attempts += -1
+            print()
+            continue
+    elif letter in all_valid_guessed_words:  # If word already guessed, don'r count it against tries.
+            print("You already typed this letter")
+            print()
+            no_of_attempts += -1
+            continue
+    all_valid_guessed_words.append(letter)
     for i in range (0,len(computed_word)):
+        
+        
         if letter not in computed_word:
             letter_present = 0
             if no_of_attempts == 8:
                 print("No such letter in the word")
                 break
             break
+        if letter not in correctely_guessed_words and no_of_attempts == 8:
+            print("No such letter in the word")
+            break 
         if computed_word[i] == letter:
             letter_present = 1
             word_aldready_guessed = 1
@@ -44,7 +66,8 @@ while no_of_attempts < 8:
                 if computed_word[j] == letter:
                     computed_word_with_blanks[j] = letter
             if letter in correctely_guessed_words:
-                print("No improvements")
+                print("No Imporvements")
+                no_of_attempts -=1
                 break
             if letter not in correctely_guessed_words and first_word_guess != 1:
                 first_word_guess = 1
@@ -53,9 +76,7 @@ while no_of_attempts < 8:
             if letter not in correctely_guessed_words:
                 correctely_guessed_words.append(letter)
                 break
-        if computed_word[i] == letter and no_of_attempts == 8:
-            print("No improvements")
-            break            
+                   
 
     if letter_present == 0:
         print("No such letter in the word")
